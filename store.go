29e2383396ebdfee872f90d4381cf02eb3423caa
Ben Tranter <bwtrante@lakeheadu.ca>
2016-01-18 23:39:06 -0500

    Import driver, use better naming

--- store.go
+++ store.go
@@ -4,19 +4,20 @@ import (
 	"database/sql"
 
 	"github.com/bentranter/password"
-	// "github.com/lib/pq"
+	// Use the Postgres DB driver
+	_ "github.com/lib/pq"
 )
 
 var defaultStore = newDB()
 
-// PqStore is a reference to the password store.
-type PqStore struct {
+// PgStore is a reference to the password store.
+type PgStore struct {
 	DB *sql.DB
 }
 
 // Store stores the given id and secret in Postgres. It will hash the secret
 // using bcrypt before storing it.
-func (p *PqStore) Store(id string, secret string) (string, error) {
+func (p *PgStore) Store(id string, secret string) (string, error) {
 	hashedSecret, err := password.Hash(secret)
 	if err != nil {
 		return "", err
@@ -28,20 +29,20 @@ func (p *PqStore) Store(id string, secret string) (string, error) {
 }
 
 // Retrieve retrieves from Postgres the hashed secret given an id and secret.
-func (p *PqStore) Retrieve(id string, secret string) (string, error) {
+func (p *PgStore) Retrieve(id string, secret string) (string, error) {
 	p.DB.Query("")
 
 	return "", nil
 }
 
 // newDB returns a default DB
-func newDB() *PqStore {
+func newDB() *PgStore {
 	db, err := sql.Open("postgres", "user=ppc dbname=ppc sslmode=verify-full")
 	if err != nil {
 		panic(err)
 	}
 
-	return &PqStore{
+	return &PgStore{
 		DB: db,
 	}
 }
